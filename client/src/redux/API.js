5fd5d71c00b07dc6197ad1a2b4fda7fbe448d811
Adeeb Ahmad <adeebsiddiqui77@gmail.com>
2024-02-01 21:43:05 +0530

    updated

--- client/src/redux/API.js
+++ client/src/redux/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://renting-carnival.onrender.com/",
-  // baseURL: "http://localhost:4000",
+  // baseURL: "https://renting-carnival.onrender.com/",
+  baseURL: "http://localhost:4000",
 });
