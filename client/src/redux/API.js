2531e97aaa6356e26849550c72565cd89035fc1f
Adeeb Ahmad <adeebsiddiqui77@gmail.com>
2024-02-06 15:39:19 +0530

    Update API.js

--- client/src/redux/API.js
+++ client/src/redux/API.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 // const BASE_URL =  "https://renting-carnival.onrender.com/"
-// const BASE_URL = "https://renting-carnival-api.onrender.com"
+const BASE_URL = "https://renting-carnival-api.onrender.com"
 
-const BASE_URL = "http://localhost:4000"
+// const BASE_URL = "http://localhost:4000"
 
 export default axios.create({
   baseURL: BASE_URL
