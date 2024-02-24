f84cb14691dd3d1121474b6f27f291c281c23b06
Adeeb Ahmad <adeebsiddiqui77@gmail.com>
2024-02-25 04:32:46 +0530

    Update Buy.jsx

--- client/src/component/Buy.jsx
+++ client/src/component/Buy.jsx
@@ -15,27 +15,27 @@ const Buy = () => {
         Choose from over 2000 Products.
       </p>
       <div className="flex flex-col sm:flex-row justify-center items-center">
-        <NavLink to="/furniture">
+        <NavLink to="/category/Furniture">
           <button className="flex flex-col items-center m-4 p-4 hover:shadow-md hover:scale-90 hover:border hover:border-gray-500 hover:transition duration-300">
             <img src={furniture} alt="furniture-image" className="m-10"></img>
             <span>Furniture</span>
           </button>
         </NavLink>
-        <NavLink to='/decorative-items'>
-        <button className="flex flex-col items-center m-4 p-4 hover:shadow-md hover:scale-90 hover:border hover:border-gray-500 hover:transition duration-300">
-          <img src={appliances} alt="decorative items" className="m-10"></img>
-          <span>Decorative Items</span>
-        </button>
+        <NavLink to='/category/Decorative-Items'>
+          <button className="flex flex-col items-center m-4 p-4 hover:shadow-md hover:scale-90 hover:border hover:border-gray-500 hover:transition duration-300">
+            <img src={appliances} alt="decorative items" className="m-10"></img>
+            <span>Decorative Items</span>
+          </button>
         </NavLink>
 
-        <NavLink to="/home-appliance">
+        <NavLink to="category/Home-Appliance">
           <button className="flex flex-col items-center m-4 p-4 hover:shadow-md hover:scale-90 hover:border hover:border-gray-500 hover:transition duration-300">
             <img src={stars} alt="decorations" className="m-10"></img>
             <span>Home Appliances</span>
           </button>
         </NavLink>
 
-        <NavLink to="/vehicles">
+        <NavLink to="category/Vehicles">
           <button className="flex flex-col items-center m-4 p-4 hover:shadow-md hover:scale-90 hover:border hover:border-gray-500 hover:transition duration-300">
             <img src={bike} alt="Vehicles" className="m-10"></img>
             <span>Vehicles</span>
